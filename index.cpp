d570ce7f3e82fbe5bfa40d1d5588b1a482ddd8a3
Joe Fowler <scrumplesplunge@gmail.com>
2025-03-05 21:44:41 +0000

    Ignore files which can't have their names converted to std::string.

--- index.cpp
+++ index.cpp
@@ -161,13 +161,25 @@ class Indexer {
         ".js",
         ".md",
         ".py",
-        // ".txt",  // Need to figure out why uncommenting this causes a crash.
+        ".txt",
     };
     std::vector<std::string> files;
     for (const fs::path& path : fs::recursive_directory_iterator(
              fs::current_path(),
              fs::directory_options::skip_permission_denied)) {
-      if (allowed.contains(path.extension())) files.push_back(path.string());
+      try {
+        if (allowed.contains(path.extension())) files.push_back(path.string());
+      } catch (std::system_error&) {
+        // Windows throws an exception when converting a non-ascii name to a
+        // string. We probably don't have enough non-ascii filenames for code
+        // that this matters much, so skip them.
+        //
+        // There isn't really a trivial fix for this. The easiest thing to do
+        // would be to use std::wstring for all filenames and only have to deal
+        // with casting those wide chars when reading or writing the index file.
+        // Ideally we would use UTF-8 but there is no UTF-8 overload for
+        // CreateFile, only the ascii one and the wide character one.
+      }
     }
     std::ranges::sort(files);
     const auto end = Clock::now();
